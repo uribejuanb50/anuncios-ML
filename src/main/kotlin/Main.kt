e2dbe8ebcd04de9958584521018495b8bfdd3669
JuanBernardo <uribejuanb@javeriana.edu.co>
2026-05-02 23:12:50 -0500

    info: verificación de conectividad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,8 @@ fun main() {
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
 
+    println("Hello world!")
+
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
